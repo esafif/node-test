29ec3bec0842171080d81cb944ba27bf2f871af8
esafif <esafif637@gmail.com>
2017-12-05 01:22:35 +0700

    adding expect to utils.test.js

--- utils/utils.js
+++ utils/utils.js
@@ -3,3 +3,10 @@ module.exports.add = (a, b) =>{
 }
 
 module.exports.square = (x) => x * x  ;
+
+module.exports.setName = (user, fullname) => {
+    var names = fullname.split(" ");
+    user.firstName = names[0];
+    user.lastName = names[1];
+    return user;
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,46 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
-    if(res !== 44){
-        throw new Error(`expected 44 , but got ${res}`)
-    }
+    // if(res !== 44){
+    //     throw new Error(`expected 44 , but got ${res}`)
+    // }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('Should square', () => {
     var res = utils.square(20);
-    if(res !== 400){
-        throw new Error(`expected 400, but got ${res}`)
-    }
+    expect(res).toBe(400).toBeA('number');
+});
+
+it('should expect some values' , () => {
+    //expect({name : "safif"}).toEqual({name: "safif"});
+    //expect([2,3,4]).toInclude(3);
+    //expect([2,3,4]).toExclude(5);
+    // expect({
+    //     name: "safif",
+    //     age: "22",
+    //     location: "Bandung"
+    // }).toInclude({
+    //     age:23
+    // })
+    expect({
+        name: "safif",
+        age: "22",
+        location: "Bandung"
+    }).toExclude({
+        age:23
+    })
+});
+
+it("should verify first and last name", () =>{
+    var user = {location: "Bandung", age: 22};
+    var res = utils.setName(user, "safif efendi");
+
+    //expect(user).toEqual(res);
+    expect(res).toInclude({
+        firstName: 'safif',
+        lastName: 'efendi'
+    });
 });
